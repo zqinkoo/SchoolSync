8646a6713a32454a28af2c94e69582e8188a2316
Aqlan Nor Azman <aqlanhadi@gmail.com>
2019-04-08 13:43:06 +0800

    all code perfected. moving on to performance fixes and bug searches

--- calsync.py
+++ calsync.py
@@ -4,6 +4,9 @@ from __future__ import print_function #run on py 2 & 3
 import datetime
 import pickle
 import json
+import os
+import getopt
+import sys
 import os.path
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
@@ -13,29 +16,46 @@ SCOPES = ['https://www.googleapis.com/auth/calendar']
 GMT_OFF = 'Asia/Kuala_Lumpur'
 CALID = "k26iavm5k4vpfieeg0ne8937us@group.calendar.google.com" #Taylor's Calendar ID
 WEEK_COUNT = 14
+STORE_FILE = "eveId.txt"
+SCHEDULE_JSON = "localization.json"
+HOLIDAYS_JSON = 'holidays.json'
 
 def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'adc:h', ['add', 'delete', 'calId=','help'])
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
+
     service = auth() #Authorize so that the script can write to your account
-    #testFunc()
-    addClasses(service)
-    #delAllEv(service)
+    for opt, arg in opts:
+        if opt in ('-h', '--help'):
+            printHelp()
+            sys.exit(2)
+        elif opt in ('-a', '--add'):
+            if os.path.isfile(STORE_FILE):
+                print("Existing schedule has already been loaded.")
+                sys.exit(2)
+            else:
+                print("Adding classes to " + CALID)
+                addClasses(service)
+        elif opt in ('-d', '--delete'):
+            if os.path.isfile(STORE_FILE):
+                delAllEv(service)
+            else:
+                print("No event (event IDs) exists to delete")
+                sys.exit(2)
+        else:
+            sys.exit(2)
 
-def testFunc():
-    today = datetime.datetime.now().date()
-    print(today)
-    tomorrow = today + datetime.timedelta(days=1)
-    print(tomorrow)
-
-    if today > tomorrow:
-        print("today is bigger")
-    else:
-        print("today is smaller")
+def printHelp():
+    print("Hi just chillin here")
 
 def addClasses(service):
     weekCount = 0
-    f = open("eveId.txt", "a+")
 
-    with open("localization.json","r") as schedfile:
+
+    with open(SCHEDULE_JSON,"r") as schedfile:
         schedule = json.load(schedfile)
 
     while weekCount < WEEK_COUNT:
@@ -54,28 +74,50 @@ def addClasses(service):
             #rDay is referenced day, to differentiate today and tomorrow in the context of first week
             tDay = dtConvert(str(c['start']['dateTime']))
             if tDay == rDay:
-                addToCal(rDay + datetime.timedelta(days=offset), c)
+                addToCal(rDay + datetime.timedelta(days=offset), c, service)
             elif tDay > rDay:
                 rDay = tDay
-                addToCal(rDay + datetime.timedelta(days=offset), c)
+                addToCal(rDay + datetime.timedelta(days=offset), c, service)
             elif tDay < rDay:
                 print("Next week")
 
         weekCount += 1
 
 
-def addToCal(today, item):
+def addToCal(today, c, service):
     #here u can check specific days -- holiday or not to exclude
-
     if holiday(today):
         print(str(today.strftime("%a")) + ", " + str(today) + " --Holiday")
     if not holiday(today):
         print(str(today.strftime("%a")) + ", " + str(today) + " --Class")
+        # print("Old time" + str(item['start']['dateTime']))
+        # print("new time" + newDate(item['start']['dateTime'], today))
+        curdict = {
+            'summary':c['summary'],
+            'location':c['location'],
+            'description':c['description'],
+            'start': {
+                'dateTime':newDate(c['start']['dateTime'],today),
+                #'dateTime':str(c['start']['dateTime']),
+                'timeZone': GMT_OFF,
+            },
+            'end': {
+                'dateTime':newDate(c['end']['dateTime'],today),
+                'timeZone': GMT_OFF,
+            }
+        }
+        e = service.events().insert(calendarId=CALID,
+            sendNotifications=False, body=curdict).execute()
+        with open(STORE_FILE, "a+") as f:
+            f.write(e.get('id') + '\n')
+
+def newDate(old, new):
+    time = str(old[10:])
+    return str(new)+time
 
 def holiday(today):
-    with open("holidays.json","r") as datelist:
+    with open(HOLIDAYS_JSON,"r") as datelist:
         lists = json.load(datelist)
-
     #Goes through all values of 'date' to see if today matches.
     #if it matches, return true for today is holiday
     if any(tag['date'] == str(today) for tag in lists['holidays']):
@@ -83,35 +125,14 @@ def holiday(today):
     else:
         return False
 
-    ##########################for later##########################
-    # for c in schedule['classes']:
-    #     curdict = {
-    #         'summary':c['summary'],
-    #         'location':c['location'],
-    #         'description':c['description'],
-    #         'start': {
-    #             'dateTime':str(c['start']['dateTime']),
-    #             'timeZone': GMT_OFF,
-    #         },
-    #         'end': {
-    #             'dateTime':str(c['end']['dateTime']),
-    #             'timeZone': GMT_OFF,
-    #         }
-    #     }
-    #     print(curdict['end'])
-    #     e = service.events().insert(calendarId=CALID,
-    #         sendNotifications=False, body=curdict).execute()
-    #     f.write(e.get('id') + '\n')
-    ##########################for later##########################
-
 def delAllEv(service):
-    with open("eveId.txt", "r") as fileHandler:
+    with open(STORE_FILE, "r") as fileHandler:
         line = fileHandler.readline()
         while line:
             print(line.strip())
             service.events().delete(calendarId=CALID, eventId=line.strip()).execute()
             line = fileHandler.readline()
-    open("eveId.txt","rw")
+    os.remove(STORE_FILE)
 
 def dtConvert(dateTime):
     year = int(dateTime[0:4])
